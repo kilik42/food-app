76411f66f1fdb4e675b1570dfa07de70198dc11a
kilik42 <marvinevins@yahoo.com>
2024-02-06 05:09:05 -0600

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+      {/* <Stack.Screen name="modal" options={{ presentation: 'modal' }} /> */}
     </Stack>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const MainScreen = () => {  
+    return <Rediret href ='/home' />
+
+}
